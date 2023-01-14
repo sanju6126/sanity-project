45e92940b473ed683674be6691df9977b3e79c2e
sanju <sanjusv1006@gmail.com>
2023-01-14 10:14:03 +0530

    made the database in backend and made a react proect in the shareMe frontend

--- schemas/index.js
+++ schemas/index.js
@@ -1,1 +1,12 @@
-export const schemaTypes = []
+// import pet from './pet'
+import user from './user'
+import pin from './pin'
+import comment from './comment'
+import postedBy from './postedBy'
+import save from './save'
+
+
+
+export const schemaTypes = [user,pin,comment,postedBy,save]
+
+
